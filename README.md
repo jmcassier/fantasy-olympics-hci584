5486c30a3b504c018a6730f1c8e34f4ce3f62a25
Jean-Marc <jcassier@iastate.edu>
2023-06-08 19:47:35 -0400

    start adding previous event results (archery - fencing)

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # fantasy-olympics-hci584
+
+Sources for Data
+
+ - 2008 Beijing Olympics: https://olympics.com/en/olympic-games/beijing-2008/
+
+ - 2012 London Olympics: https://olympics.com/en/olympic-games/london-2012/
+
+ - 2016 Rio Olympics: https://olympics.com/en/olympic-games/rio-2016/
+
+ - 2020 Tokyo Olympics: https://olympics.com/en/olympic-games/tokyo-2020/
--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import csv
 import pandas as pd
 import math
+import sqlite3
 
 previous_scores = pd.DataFrame(columns = ["country"])
 current_scores = pd.DataFrame(columns = ["country"])
@@ -15,6 +16,17 @@ def determine_tiers():
     read_file('./medal-data/beijing-medal-table.csv', 'beijing', previous_scores)
     read_file('./medal-data/london-medal-table.csv', 'london', previous_scores)
     read_file('./medal-data/rio-medal-table.csv', 'rio', previous_scores)
+
+    connect = sqlite3.connect('olympic_stats.db')
+    connect.execute('DROP TABLE IF EXISTS olympic_stats')
+    connect.execute('''
+        CREATE TABLE olympic_stats_test (
+            country text primary key,
+            beijing int,
+            london int,
+            rio int
+        );
+    ''')
 
     for index, row in previous_scores.iterrows():
         beijing = 0 if math.isnan(row[1]) else row[1]
@@ -31,102 +43,114 @@ def determine_tiers():
             tiers['D'].append(previous_scores.at[index, 'country'])
         elif avg >= 13:
             tiers['E'].append(previous_scores.at[index, 'country'])
+        
+        connect.execute('''
+            INSERT INTO olympic_stats_test
+            VALUES (?, ?, ?, ?);
+            ''',
+            (row.country, beijing, london, rio)
+        )
+        connect.commit()
+    
+    abc = connect.execute("SELECT * from olympic_stats_test")
+    print(abc)
+    connect.close()
 
 def parse_current_scores():
     read_file('./medal-data/tokyo-medal-table.csv', 'tokyo', current_scores)
 
-def read_file(file: str, olympic_cycle: str, table):
+def read_file(file: str, column_name: str, table):
     with open(file, newline = '') as medal_table:
-        table[olympic_cycle] = math.nan
+        table[column_name] = math.nan
         reader = csv.reader(medal_table, quotechar = '|')
         for row in reader:
             score = (int(row[1]) * 3) + (int(row[2]) * 2) + int(row[3])
             country = row[0]
             if (country in table['country'].unique()):
                 country_idx = table.index[table['country'] == country][0]
-                table.at[country_idx, olympic_cycle] = score
+                table.at[country_idx, column_name] = score
             else:
-                table.loc[len(table)] = {'country': country, olympic_cycle: score}
+                table.loc[len(table)] = {'country': country, column_name: score}
 
                 
 
 determine_tiers()
 parse_current_scores()
 
-while(True):
-    if (input("Do you have an new entry to make? (y/n): ") == "n"):
-        break
+# while(True):
+#     if (input("Do you have an new entry to make? (y/n): ") == "n"):
+#         break
 
-    name = input("Enter your name: ")
-    while (name in players['name'].unique()):
-        name = input("This player already exists. Please enter a new name: ")
+#     name = input("Enter your name: ")
+#     while (name in players['name'].unique()):
+#         name = input("This player already exists. Please enter a new name: ")
     
-    score = 0
+#     score = 0
 
-    print("Select 2 Tier A teams. They are as follows: ", tiers['A'])
-    tier_a1 = input("First Tier A Team: ")
-    while (tier_a1 not in tiers['A']):
-        tier_a1 = input("This country is not in Tier A. Please select a different team: ")
-    score += current_scores.loc[current_scores['country'] == tier_a1, 'tokyo'].tolist()[0]
-    print(score)
+#     print("Select 2 Tier A teams. They are as follows: ", tiers['A'])
+#     tier_a1 = input("First Tier A Team: ")
+#     while (tier_a1 not in tiers['A']):
+#         tier_a1 = input("This country is not in Tier A. Please select a different team: ")
+#     score += current_scores.loc[current_scores['country'] == tier_a1, 'tokyo'].tolist()[0]
+#     print(score)
 
-    tier_a2 = input("Second Tier A Team: ")
-    while ((tier_a2 == tier_a1) or (tier_a2 not in tiers['A'])):
-        if tier_a2 == tier_a1:
-            tier_a2 = input("You have already selected this country. Please select a different team: ")
-        else:
-            tier_a2 = input("This country is not in Tier A. Please select a different team: ")
-    score += current_scores.loc[current_scores['country'] == tier_a2, 'tokyo'].tolist()[0]
-    print(score)
+#     tier_a2 = input("Second Tier A Team: ")
+#     while ((tier_a2 == tier_a1) or (tier_a2 not in tiers['A'])):
+#         if tier_a2 == tier_a1:
+#             tier_a2 = input("You have already selected this country. Please select a different team: ")
+#         else:
+#             tier_a2 = input("This country is not in Tier A. Please select a different team: ")
+#     score += current_scores.loc[current_scores['country'] == tier_a2, 'tokyo'].tolist()[0]
+#     print(score)
 
-    print("Select 1 Tier B teams. They are as follows: ", tiers['B'])
-    tier_b = input("Tier B Team: ")
-    while (tier_b not in tiers['B']):
-        tier_b = input("This country is not in Tier B. Please select a different team: ")
-    score += current_scores.loc[current_scores['country'] == tier_b, 'tokyo'].tolist()[0]
-    print(score)
+#     print("Select 1 Tier B teams. They are as follows: ", tiers['B'])
+#     tier_b = input("Tier B Team: ")
+#     while (tier_b not in tiers['B']):
+#         tier_b = input("This country is not in Tier B. Please select a different team: ")
+#     score += current_scores.loc[current_scores['country'] == tier_b, 'tokyo'].tolist()[0]
+#     print(score)
 
-    print("Select 1 Tier C teams. They are as follows: ", tiers['C'])
-    tier_c = input("Tier C Team: ")
-    while (tier_c not in tiers['C']):
-        tier_c = input("This country is not in Tier C. Please select a different team: ")
-    score += current_scores.loc[current_scores['country'] == tier_c, 'tokyo'].tolist()[0]
-    print(score)
+#     print("Select 1 Tier C teams. They are as follows: ", tiers['C'])
+#     tier_c = input("Tier C Team: ")
+#     while (tier_c not in tiers['C']):
+#         tier_c = input("This country is not in Tier C. Please select a different team: ")
+#     score += current_scores.loc[current_scores['country'] == tier_c, 'tokyo'].tolist()[0]
+#     print(score)
 
-    print("Select 1 Tier D teams. They are as follows: ", tiers['D'])
-    tier_d = input("Tier D Team: ")
-    while (tier_d not in tiers['D']):
-        tier_d = input("This country is not in Tier D. Please select a different team: ")
-    score += current_scores.loc[current_scores['country'] == tier_d, 'tokyo'].tolist()[0]
-    print(score)
+#     print("Select 1 Tier D teams. They are as follows: ", tiers['D'])
+#     tier_d = input("Tier D Team: ")
+#     while (tier_d not in tiers['D']):
+#         tier_d = input("This country is not in Tier D. Please select a different team: ")
+#     score += current_scores.loc[current_scores['country'] == tier_d, 'tokyo'].tolist()[0]
+#     print(score)
 
-    print("Select 3 Tier E teams. They are as follows: ", tiers['E'])
-    tier_e1 = input("First Tier E Team: ")
-    while (tier_e1 not in tiers['E']):
-        tier_e1 = input("This country is not in Tier E. Please select a different team: ")
-    score += current_scores.loc[current_scores['country'] == tier_e1, 'tokyo'].tolist()[0]
-    print(score)
+#     print("Select 3 Tier E teams. They are as follows: ", tiers['E'])
+#     tier_e1 = input("First Tier E Team: ")
+#     while (tier_e1 not in tiers['E']):
+#         tier_e1 = input("This country is not in Tier E. Please select a different team: ")
+#     score += current_scores.loc[current_scores['country'] == tier_e1, 'tokyo'].tolist()[0]
+#     print(score)
 
-    tier_e2 = input("Second Tier E Team: ")
-    while ((tier_e2 == tier_e1) or (tier_e2 not in tiers['E'])):
-        if tier_e2 == tier_e1:
-            tier_e2 = input("You have already selected this country. Please select a different team: ")
-        else:
-            tier_e2 = input("This country is not in Tier E. Please select a different team: ")
-    score += current_scores.loc[current_scores['country'] == tier_e2, 'tokyo'].tolist()[0]
-    print(score)
+#     tier_e2 = input("Second Tier E Team: ")
+#     while ((tier_e2 == tier_e1) or (tier_e2 not in tiers['E'])):
+#         if tier_e2 == tier_e1:
+#             tier_e2 = input("You have already selected this country. Please select a different team: ")
+#         else:
+#             tier_e2 = input("This country is not in Tier E. Please select a different team: ")
+#     score += current_scores.loc[current_scores['country'] == tier_e2, 'tokyo'].tolist()[0]
+#     print(score)
     
-    tier_e3 = input("Third Tier E Team: ")
-    while ((tier_e3 == tier_e1) or (tier_e3 == tier_e2) or (tier_e3 not in tiers['E'])):
-        if tier_e3 not in tiers['E']:
-            tier_e3 = input("This country is not in Tier E. Please select a different team: ")
-        else:
-            tier_e3 = input("You have already selected this country. Please select a different team: ")
-    score += current_scores.loc[current_scores['country'] == tier_e3, 'tokyo'].tolist()[0]
-    print(score)
+#     tier_e3 = input("Third Tier E Team: ")
+#     while ((tier_e3 == tier_e1) or (tier_e3 == tier_e2) or (tier_e3 not in tiers['E'])):
+#         if tier_e3 not in tiers['E']:
+#             tier_e3 = input("This country is not in Tier E. Please select a different team: ")
+#         else:
+#             tier_e3 = input("You have already selected this country. Please select a different team: ")
+#     score += current_scores.loc[current_scores['country'] == tier_e3, 'tokyo'].tolist()[0]
+#     print(score)
             
     
-    players.loc[len(players)] = {'name': name, 'tier a': (tier_a1, tier_a2), 'tier b': tier_b, 'tier c': tier_c, 'tier d': tier_d, 'tier e': (tier_e1, tier_e2, tier_e3), 'score': score}
-    players.sort_values(by = ['score'], inplace = True, ascending = False)
-    print(players.to_string())
+#     players.loc[len(players)] = {'name': name, 'tier a': (tier_a1, tier_a2), 'tier b': tier_b, 'tier c': tier_c, 'tier d': tier_d, 'tier e': (tier_e1, tier_e2, tier_e3), 'score': score}
+#     players.sort_values(by = ['score'], inplace = True, ascending = False)
+#     print(players.to_string())
     
